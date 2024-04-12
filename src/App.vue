d11f4c348af59f836dd89b3385e2701018ca6075
Efendo <efe_mustafa.oener@icloud.com>
2024-04-12 21:46:24 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav class="bg-white drop-shadow-md flex">
+    <img class="w-16 h-32 lg:w-32 lg:h-32 my-auto lg:m-4" src="./assets/lsid.svg" />
+    <div class=" grid grid-cols-8  gap-2 lg:gap-0">
+    <RouterLink class="text-center my-auto p-9 mr-8 lg:mr-0 text-[0.8rem] lg:text-[1rem] font-bold active:bg-gray-300 lg:hover:bg-gray-200" to="/">Home</RouterLink>
+    <RouterLink class="text-center my-auto text-[0.8rem] mx-4 lg:mx-0 p-9 lg:text-[1rem] font-bold active:bg-gray-300 lg:hover:bg-gray-200" to="/about">About</RouterLink>
+  </div>
+  </nav>
+  <RouterView />
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-center">
+        <div class=" m-10 p-16 rounded-3xl text-white font-bold text-center bg-gradient-to-t from-emerald-400 to-emerald-700">
+            <h1 class="text-3xl" >A Passion for Video Game Creation</h1>
+            <br>
+            <h1 class=" font-normal text-xl">Here at LSID Studios We Love Making Games</h1>
+        </div>
+        <br>
+        <p class="mx-8">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+    </div>
+</template>
